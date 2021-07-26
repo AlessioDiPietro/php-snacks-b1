3493eb3aba74a088ccc54e7dd425e8943341bd7a
AlessioDPT <alessio.recos@gmail.com>
2021-07-26 14:48:43 +0200

    ADD- array partite basket

--- snack1.php
+++ snack1.php
@@ -1,6 +1,43 @@
 <?php
 
-
+$basket_day = [
+    [
+        "home_team" => "Hawks",
+        "visit_team" => "Wizards",
+        "home_point" =>  120,
+        "visit_point" => 116
+    ],
+    [
+        "home_team" => "Nets",
+        "visit_team" => "Spurs",
+        "home_point" =>  128,
+        "visit_point" => 116
+    ],
+    [
+        "home_team" => "Cavaliers",
+        "visit_team" => "Celtics",
+        "home_point" =>  102,
+        "visit_point" => 94
+    ],
+    [
+        "home_team" => "Mavericks",
+        "visit_team" => "Pellicans",
+        "home_point" =>  125,
+        "visit_point" => 107
+    ],
+    [
+        "home_team" => "Lakers",
+        "visit_team" => "Rockets",
+        "home_point" =>  124,
+        "visit_point" => 122
+    ],
+    [
+        "home_team" => "Bulls",
+        "visit_team" => "Raptors",
+        "home_point" =>  114,
+        "visit_point" => 102
+    ],
+]
 
 
 ?>
